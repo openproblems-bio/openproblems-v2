7c89329a2bae233f0031c5110cf3c93986ead9ec
KaiWaldrant <kai.waldrant@outlook.com>
2022-12-15 16:24:13 +0100

    add lmds method

--- src/joint_embedding/control_methods/zeros_embed/script.py
+++ src/joint_embedding/control_methods/zeros_embed/script.py
@@ -1,10 +1,10 @@
 import anndata
 import numpy as np
+from scipy import sparse
 
 ## VIASH START
 par = {
     "input_mod1": "resources_test/joint_embedding/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter.mod1.h5ad",
-    "input_mod2": "resources_test/joint_embedding/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter.mod2.h5ad",
     "output": "tmp/output_prediction.h5ad",
     "n_dims": 1,
 }
@@ -18,6 +18,10 @@ print("Saving output")
 adata_out = anndata.AnnData(
     X=X,
     obs=adata_mod1.obs,
-    uns={"dataset_id": adata_mod1.uns["dataset_id"], "method_id": "dummy_zeros"},
+    uns={"dataset_id": adata_mod1.uns["dataset_id"], "method_id": "zeros_embed"},
+    obsm={"X_emb": sparse.csr_matrix(X) }
 )
+
+del adata_out.X
+
 adata_out.write_h5ad(par["output"], compression="gzip")
