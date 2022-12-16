1b47472afdd66614ad1a897c30491c3ebba82364
KaiWaldrant <kai.waldrant@outlook.com>
2022-12-16 21:59:31 +0100

    add cc_cons metric

--- src/joint_embedding/metrics/cc_cons/script.py
+++ src/joint_embedding/metrics/cc_cons/script.py
@@ -1,5 +1,4 @@
 import pprint
-import scanpy as sc
 import anndata as ad
 import scib
 
@@ -10,46 +9,46 @@ par = dict(
     output="resources_test/common/joint_embedding/score_cc_cons.h5ad",
     debug=False
 )
-
 ## VIASH END
+
 
 if par['debug']:
     pprint.pprint(par)
 
-input_prediction = par['input_prediction']
-input_solution = par['input_solution']
-output = par['output']
-
 print("Read prediction anndata")
-adata = ad.read_h5ad(input_prediction)
+adata = ad.read_h5ad(par['input_prediction'])
 dataset_id = adata.uns['dataset_id']
 
 print("Read solution anndata")
-adata_solution = ad.read_h5ad(input_solution)
+adata_solution = ad.read_h5ad(par['input_solution'])
+organism = adata_solution.uns['organism']
 
 print('Transfer obs annotations')
 adata.obs['batch'] = adata_solution.obs['batch'][adata.obs_names]
 adata.obs['cell_type'] = adata_solution.obs['cell_type'][adata.obs_names]
+recompute_cc = 'S_score' not in adata_solution.obs_keys() or \
+               'G2M_score' not in adata_solution.obs_keys()
 
 print('Compute score')
-score = scib.me.silhouette_batch(
-    adata,
+score = scib.me.cell_cycle(
+    adata_pre=adata_solution,
+    adata_post=adata,
     batch_key='batch',
-    group_key='cell_type',
     embed='X_emb',
-    verbose=False
+    recompute_cc=recompute_cc,
+    organism=organism
 )
 
 # store adata with metrics
 print("Create output object")
 out = ad.AnnData(
-    uns=dict(
-        dataset_id=adata.uns['dataset_id'],
-        method_id=adata.uns['method_id'],
-        metric_ids=['asw_batch'],
-        metric_values=[score]
-    )
+    uns= {
+        "dataset_id":adata.uns['dataset_id'],
+        "method_id":adata.uns['method_id'],
+        "metric_ids":['cc_cons'],
+        "metric_values":[score],
+    }
 )
 
 print("Write output to h5ad file")
-out.write(output, compression='gzip')
+out.write(par['output'], compression='gzip')
