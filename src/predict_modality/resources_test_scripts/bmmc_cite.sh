ca22ef281c0be626be352897a866b09b1e76fd4d
KaiWaldrant <kai.waldrant@outlook.com>
2023-01-23 12:39:00 +0100

    update resurces test script

--- src/predict_modality/resources_test_scripts/bmmc_cite.sh
+++ src/predict_modality/resources_test_scripts/bmmc_cite.sh
@@ -30,26 +30,24 @@ viash run src/predict_modality/mask_dataset/config.vsh.yaml -- \
     --output_solution $DATASET_DIR/cite_solution.h5ad
 
 # run one method
-viash run src/match_modality/methods/dr_knnr_cbf/config.vsh.yaml -- \
+viash run src/predict_modality/methods/knnr_py/config.vsh.yaml -- \
     --input_train_mod1 $DATASET_DIR/cite_train_mod1.h5ad \
     --input_train_mod2 $DATASET_DIR/cite_train_mod2.h5ad \
-    --input_train_sol $DATASET_DIR/cite_train_sol.h5ad \
     --input_test_mod1 $DATASET_DIR/cite_test_mod1.h5ad \
-    --input_test_mod2 $DATASET_DIR/cite_test_mod2.h5ad \
-    --output $DATASET_DIR/dr_knnr_cbf.h5ad
+    --output $DATASET_DIR/knnr_py.h5ad
 
 # run one metric
-viash run src/match_modality/metrics/aupr/config.vsh.yaml -- \
-    --input_prediction $DATASET_DIR/dr_knnr_cbf.h5ad \
+viash run src/predict_modality/metrics/mse/config.vsh.yaml -- \
+    --input_prediction $DATASET_DIR/knnr_py.h5ad \
     --output_solution $DATASET_DIR/cite_solution.h5ad
-    --output $DATASET_DIR/aupr.h5ad
+    --output $DATASET_DIR/mse.h5ad
 
 # run benchmark
 export NXF_VER=22.04.5
 
 nextflow \
   run . \
-  -main-script src/match_modality/workflows/run/main.nf \
+  -main-script src/predict_modality/workflows/run/main.nf \
   -profile docker \
   --id bmmc_cite \
   --dataset_id bmmc_site \
