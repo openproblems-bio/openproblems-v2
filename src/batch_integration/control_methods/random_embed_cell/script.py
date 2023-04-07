87d42f4b5438576bb680010399ef690b74ebeb35
KaiWaldrant <kai.waldrant@outlook.com>
2023-04-07 11:18:38 +0200

    add random embedding

---
+++ src/batch_integration/control_methods/random_embed_cell/script.py
@@ -0,0 +1,39 @@
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+import anndata as ad
+import yaml
+
+## VIASH START
+
+par = {
+    'input': 'resources_test/batch_integration/pancreas/unintegrated.h5ad',
+    'output': 'output.h5ad',
+    'jitter': None
+}
+
+meta = { 
+    'functionality': 'foo',
+    'config': 'bar'
+}
+
+## VIASH END
+
+with open(meta['config'], 'r', encoding="utf8") as file:
+    config = yaml.safe_load(file)
+
+output_type = config["functionality"]["info"]["output_type"]
+
+print('Read input', flush=True)
+input = ad.read_h5ad(par['input'])
+
+
+print('processing data', flush=True)
+input.obsm['X_emb'] = OneHotEncoder().fit_transform(
+    LabelEncoder().fit_transform(input.obs["label"])[:, None]
+)
+
+print("Store outputs", flush=True)
+input.uns['output_type'] = output_type
+input.uns['hvg'] = par['hvg']
+input.uns['method_id'] = meta['functionality_name']
+input.write_h5ad(par['output'], compression='gzip')
