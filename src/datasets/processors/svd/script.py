2298f8ca6c882afb29e2271907a354a1d6d99a78
Kai Waldrant <kai@data-intuitive.com>
2023-07-05 22:22:48 +0200

    add svd processor

---
+++ src/datasets/processors/svd/script.py
@@ -0,0 +1,28 @@
+import anndata as ad
+import sklearn.decomposition
+
+
+## VIASH START
+par = {
+    "input": "resources_test/common/multimodal/normalized_mod1.h5ad",
+    "output": "output.h5ad",
+    "layer_input": "normalized",
+    "obsm_embedding": "X_svd",
+    "num_components": 100,
+}
+## VIASH END
+
+print(">> Load data", flush=True)
+adata = ad.read(par["input"])
+
+print("check parameters", flush=True)
+n_svd = min([par["num_components"], min(adata.layers["normalized"].shape) - 1])
+
+print(">> Run SVD", flush=True)
+svd = sklearn.decomposition.TruncatedSVD(n_svd).fit_transform(adata.layers["normalized"])
+
+print(">> Storing output", flush=True)
+adata.obsm[par["obsm_embedding"]] = svd
+
+print(">> Writing data", flush=True)
+adata.write_h5ad(par["output"])
