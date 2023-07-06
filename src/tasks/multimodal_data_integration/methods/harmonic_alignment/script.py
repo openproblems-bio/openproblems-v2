e53af81c37d2f56e763442a3f30432cc2fd3d58f
Kai Waldrant <kai@data-intuitive.com>
2023-07-06 16:29:30 +0200

    refactor harmony method

--- src/tasks/multimodal_data_integration/methods/harmonic_alignment/script.py
+++ src/tasks/multimodal_data_integration/methods/harmonic_alignment/script.py
@@ -1,7 +1,6 @@
 print("Loading dependencies")
-import scanpy as sc
+import anndata as ad
 import harmonicalignment
-import sklearn.decomposition
 
 ## VIASH START
 par = {
@@ -12,15 +11,18 @@ par = {
   "n_pca_XY" : 100,
   "eigenvectors" : 100
 }
+
+meta = {
+    "functionality_name" : "harmonic_alignment"
+    }
 ## VIASH END
 
 
-print("Reading input h5ad file")
-adata_mod1 = sc.read_h5ad(par["mod1"])
-adata_mod2 = sc.read_h5ad(par["mod2"])
+print("Reading input h5ad file", flush=True)
+adata_mod1 = ad.read_h5ad(par["input_mod1"])
+adata_mod2 = ad.read_h5ad(par["input_mod2"])
 
-print("Check parameters")
-n_svd = min([par["n_svd"], min(adata_mod1.layers["normalized"].shape) - 1, min(adata_mod2.layers["normalized"].shape) - 1])
+print("Check parameters", flush=True)
 n_eigenvectors = par["n_eigenvectors"]
 n_pca_XY = par["n_pca_XY"]
 
@@ -30,28 +32,20 @@ if adata_mod1.layers["normalized"].shape[0] <= n_pca_XY:
     n_pca_XY = None
 
 
-print("Performing PCA reduction")
-mod1_pca = sklearn.decomposition.TruncatedSVD(n_svd).fit_transform(adata_mod1.layers["normalized"])
-mod2_pca = sklearn.decomposition.TruncatedSVD(n_svd).fit_transform(adata_mod2.layers["normalized"])
-
-print("Running Harmonic Alignment")
+print("Running Harmonic Alignment", flush=True)
 ha_op = harmonicalignment.HarmonicAlignment(
     n_filters=8, n_pca_XY=n_pca_XY, n_eigenvectors=n_eigenvectors
 )
-ha_op.align(mod1_pca, mod2_pca)
+ha_op.align(adata_mod1.obsm["X_svd"], adata_mod2.obsm["X_svd"])
 XY_aligned = ha_op.diffusion_map(n_eigenvectors=n_eigenvectors)
 
-print("Storing output data structures")
+print("Storing output data structures", flush=True)
 
-adata= sc.AnnData(
-    uns={
-        
-    }
-)
+adata_mod1.obsm["aligned"] = XY_aligned[: adata_mod1.obsm["X_svd"].shape[0]]
+adata_mod2.obsm["aligned"] = XY_aligned[-adata_mod2.obsm["X_svd"].shape[0] :]
 
-adata.obsm["aligned"] = XY_aligned[: X_pca.shape[0]]
-adata.obsm["mode2_aligned"] = XY_aligned[X_pca.shape[0] :]
-
-print("Write output to file")
-adata.uns["method_id"] = "harmonic_alignment"
-adata.write_h5ad(par["output"], compression = "gzip")
+print("Write output to file", flush=True)
+adata_mod1.uns["method_id"] = meta["functionality_name"]
+adata_mod2.uns["method_id"] = meta["functionality_name"]
+adata_mod1.write_h5ad(par["output"], compression = "gzip")
+adata_mod2.write_h5ad(par["output"], compression = "gzip")
