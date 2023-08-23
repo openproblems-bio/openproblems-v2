06c9dc64c08a84068c7d52b22d0d526d89426a7a
Robrecht Cannoodt <rcannood@gmail.com>
2023-08-23 14:23:17 +0200

    split fastmnn into fastmnn_feature and fastmnn_embed

--- src/tasks/batch_integration/methods/fastmnn_feature/script.R
+++ src/tasks/batch_integration/methods/fastmnn_feature/script.R
@@ -8,8 +8,7 @@ suppressPackageStartupMessages({
 ## VIASH START
 par <- list(
   input = 'resources_test/batch_integration/pancreas/unintegrated.h5ad',
-  output = 'output.h5ad',
-  hvg = FALSE
+  output = 'output.h5ad'
 )
 meta <- list(
   functionality_name = "mnn_correct_feature"
@@ -18,12 +17,6 @@ meta <- list(
 
 cat("Read input\n")
 adata <- anndata::read_h5ad(par$input)
-
-# don't subset when return_type is not "feature"
-if ("hvg" %in% names(par) && par$hvg) {
-  cat("Select HVGs\n")
-  adata <- adata[, adata$var[["hvg"]]]
-}
 
 # TODO: pass output of 'multiBatchNorm' to fastMNN
 
@@ -34,20 +27,16 @@ out <- suppressWarnings(batchelor::fastMNN(
 ))
 
 cat("Reformat output\n")
-obsm <- SingleCellExperiment::reducedDim(out, "corrected")
-adata$obsm[["X_emb"]] <- obsm
-# return_type == "feature" is currently not working in fastMNN
+# reusing the same script for fastmnn_embed and fastmnn_feature
+return_type <- gsub("fastmnn_", "", meta[["functionality_name"]])
 
-# # reusing the same script for mnn_correct and mnn_correct_feature
-# return_type <- gsub("mnn_correct_", "", meta[["functionality_name"]])
-
-# if (return_type == "feature") {
-#   layer <- SummarizedExperiment::assay(out, "corrected")
-#   adata$layers[["corrected_counts"]] <- as(t(layer), "sparseMatrix")
-# } else if (return_type == "embedding") {
-#   obsm <- SingleCellExperiment::reducedDim(out, "corrected")
-#   adata$obsm[["X_emb"]] <- obsm
-# }
+if (return_type == "feature") {
+  layer <- as(SummarizedExperiment::assay(out, "reconstructed"), "dgCMatrix")
+  adata$layers[["corrected_counts"]] <- t(layer)
+} else if (return_type == "embedding") {
+  obsm <- SingleCellExperiment::reducedDim(out, "corrected")
+  adata$obsm[["X_emb"]] <- obsm
+}
 
 cat("Store outputs\n")
 adata$uns[["method_id"]] <- meta$functionality_name
