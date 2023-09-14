ab199b0b6951b659764943bf6dee88a45fe2ae90
Robrecht Cannoodt <rcannood@gmail.com>
2023-09-14 14:00:11 +0200

    refactor test resources

---
+++ src/datasets/resource_test_scripts/bmmc_x_starter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+NEURIPS2021_URL="https://github.com/openproblems-bio/neurips2021_multimodal_viash/raw/main/resources_test/common"
+DATASET_DIR="resources_test/common"
+
+SUBDIR="$DATASET_DIR/bmmc_cite_starter"
+mkdir -p "$SUBDIR"
+wget "$NEURIPS2021_URL/openproblems_bmmc_cite_starter/openproblems_bmmc_cite_starter.output_rna.h5ad" \
+  -O "$SUBDIR/dataset_rna.h5ad"
+wget "$NEURIPS2021_URL/openproblems_bmmc_cite_starter/openproblems_bmmc_cite_starter.output_mod2.h5ad" \
+  -O "$SUBDIR/dataset_adt.h5ad"
+
+SUBDIR="$SUBDIR"
+mkdir -p "$SUBDIR"
+wget "$NEURIPS2021_URL/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter.output_rna.h5ad" \
+  -O "$SUBDIR/dataset_rna.h5ad"
+wget "$NEURIPS2021_URL/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter.output_mod2.h5ad" \
+  -O "$SUBDIR/dataset_atac.h5ad"
