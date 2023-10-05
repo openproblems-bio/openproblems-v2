8342fb68cb426ab5207abe6650920a6a52a6b355
Kai Waldrant <kai@data-intuitive.com>
2023-10-05 15:47:30 +0200

    add nf-tower scripts batch_integration

--- src/tasks/batch_integration/nf_tower_scripts/run_test_on_tower.sh
+++ src/tasks/batch_integration/nf_tower_scripts/run_test_on_tower.sh
@@ -4,10 +4,11 @@ DATASET_DIR=resources_test/batch_integration/pancreas
 
 # try running on nf tower
 cat > /tmp/params.yaml << HERE
-id: pancreas_subsample
-input: s3://openproblems-data/$DATASET_DIR/dataset.h5ad
-output: scores.tsv
-publish_dir: s3://openproblems-nextflow/output_test/v2/batch_integration
+d: batch_integration_test
+input_dir: s3://openproblems-data/resources_test/batch_integration/pancreas
+rename_keys: 'input_dataset:output_dataset,input_solution:output_solution'
+settings: '{"output": "scores.tsv"}'
+publish_dir: s3://openproblems-nextflow/output_test/v2/batch_integration/
 HERE
 
 cat > /tmp/nextflow.config << HERE
@@ -19,8 +20,9 @@ HERE
 tw launch https://github.com/openproblems-bio/openproblems-v2.git \
   --revision main_build \
   --pull-latest \
-  --main-script src/tasks/batch_integration/workflows/run/main.nf \
+  --main-script target/nextflow/batch_integration/workflows/run_benchmark/main.nf \
   --workspace 53907369739130 \
   --compute-env 7IkB9ckC81O0dgNemcPJTD \
   --params-file /tmp/params.yaml \
+  --entry-name auto \
   --config /tmp/nextflow.config
