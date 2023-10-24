2198f5cfdc5f8f21a5554c4a152d36dbfdd893f0
Robrecht Cannoodt <rcannood@gmail.com>
2023-10-24 07:07:31 +0200

    added more print

--- src/common/helper_functions/read_and_merge_yaml.py
+++ src/common/helper_functions/read_and_merge_yaml.py
@@ -8,8 +8,10 @@ def read_and_merge_yaml(path):
     Arguments:
     path -- Path to the Viash YAML"""
     import ruamel.yaml as yaml
+    print("imported ruayaml", flush=True)
     with open(path, 'r') as stream:
         data = yaml.safe_load(stream)
+    print("loaded yaml", flush=True)
     return _ram_process_merge(data, path)
 
 def _ram_deep_merge(dict1, dict2):
@@ -31,15 +33,24 @@ def _ram_deep_merge(dict1, dict2):
         return dict2
 
 def _ram_process_merge(data, path):
+    print("processing merge", flush=True)
     import os
+    print("imported os", flush=True)
     if isinstance(data, dict):
+        print("is dict", flush=True)
         processed_data = {k: _ram_process_merge(v, path) for k, v in data.items()}
+        print("processed data", flush=True)
 
         if "__merge__" in processed_data:
+            print("merge key found", flush=True)
             new_data_path = os.path.join(os.path.dirname(path), processed_data["__merge__"])
+            print("new data path", flush=True)
             new_data = read_and_merge_yaml(new_data_path)
+            print("new data", flush=True)
         else:
             new_data = {}
+        
+        print("returning merge", flush=True)
 
         return _ram_deep_merge(new_data, processed_data)
     elif isinstance(data, list):
