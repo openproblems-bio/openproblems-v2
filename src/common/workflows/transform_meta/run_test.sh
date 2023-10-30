4db3472522224cdb9e8f9f17201a93b804db0a23
Kai Waldrant <kai@data-intuitive.com>
2023-10-30 10:56:23 +0100

    fix common workflow

--- src/common/workflows/transform_meta/run_test.sh
+++ src/common/workflows/transform_meta/run_test.sh
@@ -20,7 +20,7 @@ fi
 export NXF_VER=22.04.5
 
 nextflow run . \
-  -main-script target/nextflow/common/workflows/get_results/main.nf \
+  -main-script target/nextflow/common/workflows/transform_meta/main.nf \
   -profile docker \
   -resume \
   --id "get_results_test" \
@@ -40,11 +40,11 @@ nextflow run . \
 
 
 # nextflow run . \
-#   -main-script target/nextflow/common/workflows/get_results/main.nf \
+#   -main-script target/nextflow/common/workflows/transform_meta/main.nf \
 #   -profile docker \
 #   -resume \
 #   -entry auto \
-#   --input_states "$DATASETS_DIR/**/state.yaml" \
+#   --input_states "$DATASETS_DIR/state.yaml" \
 #   --rename_keys 'input_scores:output_scores,input_dataset_info:output_dataset_info, input_method_configs:output_method_configs, input_metric_configs:output_metric_configs, ' \
 #   --settings '{"task_id": "batch_integration", "output_scores": "results.json", "output_method_info": "method_info.json", "output_metric_info": "metric_info.json", "output_dataset_info": "dataset_info.json", "output_task_info":"task_info.json"}' \
-#   --publish_dir "$OUTPUT_DIR" \
+#   --publish_dir "$OUTPUT_DIR"
