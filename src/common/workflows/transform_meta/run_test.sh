31478b5e2ceded10e01c9adf57b76ea8e71541b6
Kai Waldrant <kai@data-intuitive.com>
2023-10-30 15:16:18 +0100

    update transform workflow cmd

--- src/common/workflows/transform_meta/run_test.sh
+++ src/common/workflows/transform_meta/run_test.sh
@@ -31,12 +31,12 @@ nextflow run . \
   --input_execution "$DATASETS_DIR/trace.txt" \
   --input_task_info "$DATASETS_DIR/task_info.yaml" \
   --task_id "batch_integration" \
-  --output_scores "$DATASETS_DIR/results.json"\
-  --output_method_info "$DATASETS_DIR/method_info.json"\
-  --output_metric_info "$DATASETS_DIR/metric_info.json"\
-  --output_dataset_info "$DATASETS_DIR/dataset_info.json"\
-  --output_task_info "$DATASETS_DIR/task_info.json" \
-  --publish_dir "$OUTPUT_DIR" \
+  --output_scores "results.json"\
+  --output_method_info "method_info.json"\
+  --output_metric_info "metric_info.json"\
+  --output_dataset_info "dataset_info.json"\
+  --output_task_info "task_info.json" \
+  --publish_dir "$OUTPUT_DIR"
 
 
 # nextflow run . \
