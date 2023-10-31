6213df0e73a6cafc3a5b8a66a1e01d5517e4a6c1
Kai Waldrant <kai@data-intuitive.com>
2023-10-31 10:28:03 +0100

    fix common tests

--- src/tasks/batch_integration/resources_test_scripts/pancreas.sh
+++ src/tasks/batch_integration/resources_test_scripts/pancreas.sh
@@ -15,31 +15,28 @@ mkdir -p $DATASET_DIR
 
 # process dataset
 echo Running process_dataset
-export NXF_VER=22.04.5
 nextflow run . \
-  -main-script target/nextflow/batch_integration/workflows/run_benchmark/main.nf \
+  -main-script target/nextflow/batch_integration/workflows/process_datasets/main.nf \
   -profile docker \
-  -resume \
-  -c src/wf_utils/labels_ci.config \
   -entry auto \
   --input_states "$RAW_DATA/**/state.yaml" \
-  --rename_keys 'input_dataset:output_dataset,input_solution:output_solution' \
-  --settings '{"output_scores": "scores.yaml", "output_dataset_info": "dataset_info.yaml", "output_method_configs": "method_configs.yaml", "output_metric_configs": "metric_configs.yaml"}' \
+  --rename_keys 'input:output_dataset' \
+  --settings '{"output_dataset": "$id/dataset.h5ad", "output_solution": "$id/solution.h5ad"}' \
   --publish_dir "$DATASET_DIR" \
-  --output_state "state.yaml"
-# output_state should be moved to settings once workaround is solved
+  --output_state '$id/state.yaml'
+output_state should be moved to settings once workaround is solved
 
-# echo Running BBKNN
-# viash run src/tasks/batch_integration/methods/bbknn/config.vsh.yaml -- \
-#   --input $DATASET_DIR/pancreas/dataset.h5ad \
-#   --output $DATASET_DIR/pancreas/integrated_graph.h5ad
+echo Running BBKNN
+viash run src/tasks/batch_integration/methods/bbknn/config.vsh.yaml -- \
+  --input $DATASET_DIR/pancreas/dataset.h5ad \
+  --output $DATASET_DIR/pancreas/integrated_graph.h5ad
 
-# echo Running SCVI
-# viash run src/tasks/batch_integration/methods/scvi/config.vsh.yaml -- \
-#   --input $DATASET_DIR/pancreas/dataset.h5ad \
-#   --output $DATASET_DIR/pancreas/integrated_embedding.h5ad
+echo Running SCVI
+viash run src/tasks/batch_integration/methods/scvi/config.vsh.yaml -- \
+  --input $DATASET_DIR/pancreas/dataset.h5ad \
+  --output $DATASET_DIR/pancreas/integrated_embedding.h5ad
 
-# echo Running combat
-# viash run src/tasks/batch_integration/methods/combat/config.vsh.yaml -- \
-#   --input $DATASET_DIR/pancreas/dataset.h5ad \
-#   --output $DATASET_DIR/pancreas/integrated_feature.h5ad
+echo Running combat
+viash run src/tasks/batch_integration/methods/combat/config.vsh.yaml -- \
+  --input $DATASET_DIR/pancreas/dataset.h5ad \
+  --output $DATASET_DIR/pancreas/integrated_feature.h5ad
