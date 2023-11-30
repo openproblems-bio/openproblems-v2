e8bed49ea76e27896ca45ae166bc7a18792fc17c
Robrecht Cannoodt <rcannood@gmail.com>
2023-11-30 16:57:59 +0100

    add summary prints

--- src/datasets/loaders/query_cellxgene_census/script.py
+++ src/datasets/loaders/query_cellxgene_census/script.py
@@ -82,6 +82,27 @@ def write_anndata(query_data, path, compression):
 
     query_data.write_h5ad(path, compression=compression)
 
+def print_unique(adata, column):
+    formatted = "', '".join(adata.obs[column].unique())
+    logger.info(f"Unique {column}: ['{formatted}']")
+
+def print_summary(query_data):
+    logger.info(f"Resulting dataset: {query_data}")
+
+    logger.info("Summary of dataset:")
+    print_unique(query_data, "assay")
+    print_unique(query_data, "assay_ontology_term_id")
+    print_unique(query_data, "cell_type")
+    print_unique(query_data, "cell_type_ontology_term_id")
+    print_unique(query_data, "dataset_id")
+    print_unique(query_data, "development_stage")
+    print_unique(query_data, "development_stage_ontology_term_id")
+    print_unique(query_data, "disease")
+    print_unique(query_data, "disease_ontology_term_id")
+    print_unique(query_data, "tissue")
+    print_unique(query_data, "tissue_ontology_term_id")
+    print_unique(query_data, "tissue_general")
+    print_unique(query_data, "tissue_general_ontology_term_id")
 
 def main():
     # check arguments
@@ -105,6 +126,9 @@ def main():
 
     # use feature_id as var_names
     query_data.var_names = query_data.var["feature_id"]
+
+    # print summary
+    print_summary(query_data)
 
     # write output to file
     write_anndata(query_data, par["output"], par["output_compression"])
