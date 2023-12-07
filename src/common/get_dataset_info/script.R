52b050b29ca25f6d45a33be4baf375916914f04c
Kai Waldrant <kai@data-intuitive.com>
2023-12-07 11:31:19 +0100

    add dataset comp

---
+++ src/common/get_dataset_info/script.R
@@ -0,0 +1,30 @@
+library(purrr, warn.conflicts = FALSE)
+library(dplyr, warn.conflicts = FALSE)
+library(rlang, warn.conflicts = FALSE)
+
+## VIASH START
+par <- list(
+  input = "output/temp/dataset_info.yaml",
+  output = "output/metric_info.json"
+)
+## VIASH END
+
+datasets <- yaml::yaml.load_file(par$input)
+
+df <- map_df(datasets, function(dataset) {
+  info <- as_tibble(map(dataset, as.data.frame))
+}) %>%
+  rename(
+    data_url = dataset_url,
+    data_reference = datast_reference,
+    dataset_summary = description,
+    paper_reference = reference,
+  )
+
+
+jsonlite::write_json(
+  purrr::transpose(df),
+  par$output,
+  auto_unbox = TRUE,
+  pretty = TRUE
+)
