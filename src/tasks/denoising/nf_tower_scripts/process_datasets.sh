90beae10c8c7ba7f903efa2da817cb32e9a6d085
Kai Waldrant <kai@data-intuitive.com>
2023-12-07 17:13:44 +0100

    Update denoising process_datasets wf

--- src/tasks/denoising/nf_tower_scripts/process_datasets.sh
+++ src/tasks/denoising/nf_tower_scripts/process_datasets.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cat > /tmp/params.yaml << HERE
-id: batch_integration_process_datasets
+id: denoising_process_datasets
 input_states: s3://openproblems-nextflow/resources/datasets/openproblems_v1/**/state.yaml
 rename_keys: 'input:output_dataset'
-settings: '{"output_dataset": "dataset.h5ad", "output_solution": "solution.h5ad"}'
-publish_dir: s3://openproblems-nextflow/resources/batch_integration/datasets/openproblems_v1
+settings: '{"output_train": "train.h5ad", "output_test": "test.h5ad"}'
+publish_dir: s3://openproblems-nextflow/resources/denoising/datasets/openproblems_v1
 HERE
 
 cat > /tmp/nextflow.config << HERE
@@ -17,7 +17,7 @@ HERE
 tw launch https://github.com/openproblems-bio/openproblems-v2.git \
   --revision main_build \
   --pull-latest \
-  --main-script target/nextflow/batch_integration/workflows/process_datasets/main.nf \
+  --main-script target/nextflow/denoising/workflows/process_datasets/main.nf \
   --workspace 53907369739130 \
   --compute-env 7IkB9ckC81O0dgNemcPJTD \
   --params-file /tmp/params.yaml \
