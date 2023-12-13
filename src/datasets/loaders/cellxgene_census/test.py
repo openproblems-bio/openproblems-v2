f18a4cbb5f1130586d62c37339e04289c43872e0
Robrecht Cannoodt <rcannood@gmail.com>
2023-12-13 15:32:46 +0100

    fix test

--- src/datasets/loaders/cellxgene_census/test.py
+++ src/datasets/loaders/cellxgene_census/test.py
@@ -16,7 +16,7 @@ def test_cellxgene_extract_metadata_expression(run_component, tmp_path):
     output_file = tmp_path / "output.h5ad"
 
     run_component([
-        "--species", "mus_musculus",
+        "--species", "homo_sapiens",
         "--obs_value_filter", "is_primary_data == True and cell_type_ontology_term_id in ['CL:0000136', 'CL:1000311', 'CL:0002616'] and suspension_type == 'cell'",
         "--output", output_file,
         "--obs_batch", "sex,sex",
