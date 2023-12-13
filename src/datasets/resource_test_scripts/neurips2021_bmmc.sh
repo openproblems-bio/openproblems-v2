ad5846741db386d407e1ba719b714fe6aa892f2c
Kai Waldrant <kai@data-intuitive.com>
2023-12-13 22:28:09 +0100

    Add resource_test script for processing NeurIPS 2021 BMMC dataset

---
+++ src/datasets/resource_test_scripts/neurips2021_bmmc.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+DATASET_DIR="resources_test/common"
+
+SUBDIR="$DATASET_DIR/neurips2021_bmmc_cite"
+mkdir -p "$SUBDIR"
+
+#make sure the following command has been executed
+#viash ns build -q 'datasets|common' --parallel --setup cb
+
+# get the root of the directory
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+# ensure that the command below is run from the root of the repository
+cd "$REPO_ROOT"
+
+set -e
+
+mkdir -p $DATASET_DIR
+
+# download dataset
+nextflow run . \
+  -main-script target/nextflow/datasets/workflows/process_openproblems_neurips2021_bmmc/main.nf \
+  -profile docker \
+  -c src/wf_utils/labels_ci.config \
+  -resume \
+  --id neurips2021_bmmc_cite \
+  --input "resources/datasets/multimodal/cite_BMMC_processed.h5ad" \
+  --mod1 "GEX" \
+  --mod2 "ADT" \
+  --dataset_name "bmcc (CITE-Seq)" \
+  --dataset_url "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE194122" \
+  --dataset_reference "Neurips" \
+  --dataset_summary "neurips small summary" \
+  --dataset_description "neurips big description" \
+  --do_subsample true \
+  --n_obs 600 \
+  --n_vars 1500 \
+  --seed 123 \
+  --normalization_methods log_cp10k \
+  --output_rna '$id/dataset_rna.h5ad' \
+  --output_dataset_other_mod '$id/dataset_other_mod.h5ad' \
+  --output_meta_rna '$id/dataset_metadata_rna.yaml' \
+  --output_meta_other_mod '$id/dataset_metadata_other_mod.yaml' \
+  --output_state '$id/state.yaml' \
+  --publish_dir "$DATASET_DIR"
+
+# run task process dataset components
+# src/tasks/match_modalities/resources_test_scripts/scicar_cell_lines.sh
