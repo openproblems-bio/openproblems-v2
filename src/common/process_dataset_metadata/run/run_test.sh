02ff4cbe3f0133c97c5e9a02ba84c8198aee7376
Kai Waldrant <kai@data-intuitive.com>
2024-01-26 16:00:39 +0100

    Add script to extract datasets metadate file to website

--- src/common/process_dataset_metadata/run/run_test.sh
+++ src/common/process_dataset_metadata/run/run_test.sh
@@ -7,21 +7,34 @@ set -e
 REPO_ROOT=$(git rev-parse --show-toplevel)
 cd "$REPO_ROOT"
 
-# settings
-INPUT_DIR="output/meta.yaml"
-OUTPUT_DIR="output/temp/metadata"
+# TODO: Add multimodal datasets
+for LOADER in "cellxgene_census" "openproblems_v1"; do 
+  BASE_DIR="s3://openproblems-data/resources/datasets/$LOADER/"
 
-# # temp sync
-# aws s3 sync $INPUT_DIR output/temp
+  for DATASET in $(aws s3 ls $BASE_DIR); do
 
-# start the run
-NXF_VER=23.10.0 nextflow run . \
-  -main-script target/nextflow/common/process_dataset_metadata/run/main.nf \
-  -profile docker \
-  -c src/wf_utils/labels_ci.config \
-  --id "process" \
-  --input "$INPUT_DIR" \
-  --publish_dir "$OUTPUT_DIR"
+    if [ "$DATASET" == "PRE" ]; then
+      continue
+    fi
+  
+    INPUT="${BASE_DIR%/}/${DATASET%/}/log_cp10k/dataset_metadata.yaml"
+    OUTPUT_DIR="../website/datasets/$LOADER/$DATASET"
+
+    echo "Processing $LOADER - $DATASET : $INPUT"
+  # # temp sync
+  # aws s3 sync $INPUT_DIR output/temp
+
+    # start the run
+    NXF_VER=23.10.0 nextflow run . \
+      -main-script target/nextflow/common/process_dataset_metadata/run/main.nf \
+      -profile docker \
+      -c src/wf_utils/labels_ci.config \
+      --id "process" \
+      --input "$INPUT" \
+      --output_state "state.yaml" \
+      --publish_dir "$OUTPUT_DIR"
 
 # cause quarto rerender to index page when in preview mode
 # touch ../website/results/$TASK/index.qmd
+    done
+done
