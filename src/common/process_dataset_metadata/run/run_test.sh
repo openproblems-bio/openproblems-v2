6be25c1cee063cefcabf94a241320d2dc82c4495
Kai Waldrant <kai@data-intuitive.com>
2024-02-01 16:19:38 +0100

    Update with to accomodate multimodal data

--- src/common/process_dataset_metadata/run/run_test.sh
+++ src/common/process_dataset_metadata/run/run_test.sh
@@ -7,33 +7,45 @@ set -e
 REPO_ROOT=$(git rev-parse --show-toplevel)
 cd "$REPO_ROOT"
 
-# TODO: Add multimodal datasets
-for LOADER in "cellxgene_census" "openproblems_v1"; do 
-  BASE_DIR="s3://openproblems-data/resources/datasets/$LOADER/"
+DATASET_DIR="s3://openproblems-data/resources/datasets/"
+
+for LOADER in $(aws s3 ls $DATASET_DIR); do
+
+  if [ "$LOADER" == "PRE" ]; then
+    continue
+  fi
+
+  BASE_DIR="${DATASET_DIR%/}/$LOADER"
 
   for DATASET in $(aws s3 ls $BASE_DIR); do
-
+    
     if [ "$DATASET" == "PRE" ]; then
       continue
     fi
-  
-    INPUT="${BASE_DIR%/}/${DATASET%/}/log_cp10k/dataset_metadata.yaml"
-    OUTPUT_DIR="../website/datasets/$LOADER/$DATASET"
+    
+    FILE_DIR="${BASE_DIR%/}/${DATASET%/}/log_cp10k/"
+    FILES=$(aws s3 ls $FILE_DIR)
+    metafiles=$(echo "$FILES" | grep "meta" | awk '{print $NF}')
+    # metafiles=$(find $INPUT -type f -name "*meta*")
+    # echo $metafiles
 
-    echo "Processing $LOADER - $DATASET : $INPUT"
-  # # temp sync
-  # aws s3 sync $INPUT_DIR output/temp
+    for metafile in $metafiles; do
+      INPUT="${FILE_DIR%/}/$metafile"
+      OUTPUT_DIR="../website/datasets/$LOADER/$DATASET"
+      OUTPUT_FILE="${metafile%.*}.json"
+      echo "Processing $LOADER - $DATASET : $INPUT"
 
-    # start the run
-    NXF_VER=23.10.0 nextflow run . \
+      # start the 
+      NXF_VER=23.10.0 nextflow run . \
       -main-script target/nextflow/common/process_dataset_metadata/run/main.nf \
       -profile docker \
       -c src/wf_utils/labels_ci.config \
       --id "extract_metadata" \
       --input "$INPUT" \
-      --output meta.json \
+      --output "$OUTPUT_FILE" \
       --output_state "state.yaml" \
       --publish_dir "$OUTPUT_DIR"
+    done
 
 # cause quarto rerender to index page when in preview mode
 # touch ../website/results/$TASK/index.qmd
