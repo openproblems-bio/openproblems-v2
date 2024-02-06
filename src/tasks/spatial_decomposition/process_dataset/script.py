e6287460f9779dd76a1c023a2f754142fd1da41e
Nirmayi <ynirmayi@gmail.com>
2024-02-06 19:17:54 +0100

    update process_dataset

--- src/tasks/spatial_decomposition/process_dataset/script.py
+++ src/tasks/spatial_decomposition/process_dataset/script.py
@@ -1,51 +1,43 @@
 import anndata as ad
+import sys 
 
 ## VIASH START
 par = {
-    "input": "resources_test/common/pancreas/dataset.h5ad",
-    "output_spatial": "dataset.h5ad",
-    "output_single_cell": "single_cell.h5ad",
+    # "input": "resources_test/common/pancreas/dataset.h5ad",
+    "input": "resources_test/spatial_decomposition/pancreas/dataset_synthetic.h5ad",
+    "output_spatial": "spatial_masked.h5ad",
+    "output_single_cell": "single_cell_ref.h5ad",
     "output_solution": "solution.h5ad",
+}
+meta = {
+    "functionality_name": "process_dataset",
+    "resources_dir": "src/tasks/spatial_decomposition/process_dataset",
+    "config": "target/nextflow/spatial_decomposition/process_dataset/.config.vsh.yaml"
 }
 ## VIASH END
 
-print(">> Load common dataset", flush=True)
+sys.path.append(meta['resources_dir'])
+from subset_anndata import read_config_slots_info, subset_anndata
+
+print(">> Load dataset", flush=True)
 adata = ad.read_h5ad(par["input"])
 
-def _pancreas_synthetic(
-    alpha: float,
-    test: bool = False,
-    n_obs: int = 100,
-    keep_techs: Optional[List[str]] = None,
-):
-    import scanpy as sc
+print(">> Figuring out which data needs to be copied to which output file", flush=True)
+slot_info = read_config_slots_info(meta["config"])
 
-    adata = load_pancreas(test=test, keep_techs=keep_techs or ["inDrop3"])
-    sc.pp.filter_genes(adata, min_counts=10)
-    adata.obs["label"] = adata.obs["celltype"]
-
-    merged_adata = generate_synthetic_dataset(
-        adata, n_obs=n_obs, alpha=alpha, test=test
-    )
-    filter_genes_cells(merged_adata)
-    return merged_adata
-
-
-def pancreas_alpha_1(test=False, n_obs=100, keep_techs: Optional[List[str]] = None):
-    return _pancreas_synthetic(test=test, n_obs=n_obs, alpha=1, keep_techs=keep_techs)
-
-pancreas = pancreas_alpha_1()
+print(">> Split dataset by modality", flush=True)
+is_sp = adata.obs["modality"] == "sp"
+adata_sp = adata[is_sp, :].copy()
+adata_sc = adata[~is_sp, :].copy()
 
 print(">> Create dataset for methods", flush=True)
-output_dataset = ad.AnnData(
-  # <- add the data you wish to store in the output_dataset object here
-)
+output_spatial = subset_anndata(adata_sp, slot_info['output_spatial_masked'])
+output_single_cell = subset_anndata(adata_sc, slot_info['output_single_cell'])
 
 print(">> Create solution object for metrics", flush=True)
-output_solution = ad.AnnData(
-  # <- add the data you wish to store in the output_solution object here
-)
+output_solution = subset_anndata(adata_sp, slot_info['output_solution'])
 
 print(">> Write to disk", flush=True)
-output_dataset.write_h5ad(par["output_dataset"])
+output_spatial.write_h5ad(par["output_spatial"])
+output_single_cell.write_h5ad(par["output_single_cell"])
 output_solution.write_h5ad(par["output_solution"])
