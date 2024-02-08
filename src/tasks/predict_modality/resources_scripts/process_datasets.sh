dc0dee20deb120f4f6ed48cd5c7600d25beb33e7
Kai Waldrant <kai@data-intuitive.com>
2024-02-08 12:11:10 +0100

    update to param_list

--- src/tasks/predict_modality/resources_scripts/process_datasets.sh
+++ src/tasks/predict_modality/resources_scripts/process_datasets.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 cat > /tmp/params.yaml << 'HERE'
-id: predict_modality_process_datasets
+param_list:
+  - id: predict_modality_process_datasets
+    settings: '{"output_train_mod1": "$id/train_mod1.h5ad", "output_train_mod2": "$id/train_mod2.h5ad", "output_test_mod1": "$id/test_mod1.h5ad", "output_test_mod2": "$id/test_mod2.h5ad"}'
+  - id: predict_modality_process_datasets_swap
+    settings: '{"output_train_mod1": "$id/train_mod1.h5ad", "output_train_mod2": "$id/train_mod2.h5ad", "output_test_mod1": "$id/test_mod1.h5ad", "output_test_mod2": "$id/test_mod2.h5ad", "swap": true}'
+
 input_states: s3://openproblems-data/resources/datasets/**/state.yaml
 rename_keys: 'input_mod1:output_mod1,input_mod2:output_mod2'
-settings: '{"output_train_mod1": "$id/train_mod1.h5ad", "output_train_mod2": "$id/train_mod2.h5ad", "output_test_mod1": "$id/test_mod1.h5ad", "output_test_mod2": "$id/test_mod2.h5ad"}'
 output_state: "$id/state.yaml"
 publish_dir: s3://openproblems-data/resources/predict_modality/datasets
 HERE
@@ -17,28 +21,6 @@ process {
     disk = '100GB'
   }
 }
-HERE
-
-tw launch https://github.com/openproblems-bio/openproblems-v2.git \
-  --revision main_build \
-  --pull-latest \
-  --main-script target/nextflow/predict_modality/workflows/process_datasets/main.nf \
-  --workspace 53907369739130 \
-  --compute-env 1pK56PjjzeraOOC2LDZvN2 \
-  --params-file /tmp/params.yaml \
-  --entry-name auto \
-  --config /tmp/nextflow.config \
-  --labels predict_modality,process_datasets
-
-
-# Swap the modalities
-cat > /tmp/params.yaml << 'HERE'
-id: predict_modality_process_datasets
-input_states: s3://openproblems-data/resources/datasets/**/state.yaml
-rename_keys: 'input_mod1:output_mod1,input_mod2:output_mod2'
-settings: '{"output_train_mod1": "$id/train_mod1.h5ad", "output_train_mod2": "$id/train_mod2.h5ad", "output_test_mod1": "$id/test_mod1.h5ad", "output_test_mod2": "$id/test_mod2.h5ad", "swap": true}'
-output_state: "$id/state.yaml"
-publish_dir: s3://openproblems-data/resources/predict_modality/datasets
 HERE
 
 tw launch https://github.com/openproblems-bio/openproblems-v2.git \
