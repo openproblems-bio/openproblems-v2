eaad2baf1b50da15edb15ec828b2230ca3a5ccb6
Robrecht Cannoodt <rcannood@gmail.com>
2024-02-21 15:13:58 +0100

    rework simlr

--- src/tasks/dimensionality_reduction/methods/simlr/script.R
+++ src/tasks/dimensionality_reduction/methods/simlr/script.R
@@ -4,12 +4,12 @@ requireNamespace("SIMLR", quietly = TRUE)
 ## VIASH START
 par <- list(
   input = "resources_test/dimensionality_reduction/pancreas/dataset.h5ad",
-  output = "output.h5ad", 
+  output = "output.h5ad",
   n_clusters = NULL,
-  n_dim = NA,
+  n_dim = NULL,
   tuning_param = 10,
-  impute = FALSE, 
-  normalize = FALSE, 
+  impute = FALSE,
+  normalize = FALSE,
   cores_ratio = 1
 )
 meta <- list(
@@ -20,13 +20,27 @@ meta <- list(
 cat("Reading input files\n")
 input <- anndata::read_h5ad(par$input)
 
+cat("Clean up memory\n")
+normalized <- as.matrix(input$layers[["normalized"]])
+
+output <- anndata::AnnData(
+  uns = list(
+    dataset_id = input$uns[["dataset_id"]],
+    method_id = meta$functionality_name,
+    normalization_id = input$uns[["normalization_id"]]
+  ),
+  shape = input$shape
+)
+rm(input)
+gc()
+
 if (is.null(par$n_clusters)) {
   cat("Estimating the number of clusters\n")
   set.seed(1)
-  NUMC = 2:5
+  NUMC <- 2:5
   estimates <- SIMLR::SIMLR_Estimate_Number_of_Clusters(
-    X = as.matrix(input$layers[["normalized"]]), 
-    NUMC = NUMC, 
+    X = normalized,
+    NUMC = NUMC,
     cores.ratio = par$cores_ratio
   )
   n_clusters <- NUMC[which.min(estimates$K2)]
@@ -42,26 +56,15 @@ if (is.null(par$n_dim)) {
 
 cat("Running SIMLR\n")
 simlr_result <- SIMLR::SIMLR(
-  X = as.matrix(input$layers[["normalized"]]), 
-  c = n_clusters, 
-  no.dim = n_dim, 
-  k = par$tuning_param, 
-  if.impute = par$impute, 
-  normalize = par$normalize, 
+  X = normalized,
+  c = n_clusters,
+  no.dim = n_dim,
+  k = par$tuning_param,
+  if.impute = par$impute,
+  normalize = par$normalize,
   cores.ratio = par$cores_ratio
 )
-obsm_X_emb <- simlr_result$ydata
 
 cat("Write output AnnData to file\n")
-output <- anndata::AnnData(
-  uns = list(
-    dataset_id = input$uns[["dataset_id"]],
-    method_id = meta$functionality_name,
-    normalization_id = input$uns[["normalization_id"]]
-  ),
-  obsm = list(
-    X_emb = obsm_X_emb
-  ), 
-  shape = input$shape
-)
+output$obsm$X_emb <- simlr_result$ydata
 output$write_h5ad(par$output, compression = "gzip")
