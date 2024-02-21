07504e3336ce8e77a541c5a7c031417fc4cb6c44
Kai Waldrant <kai@data-intuitive.com>
2024-02-21 09:59:45 +0100

    WIP refactor novel

--- src/tasks/predict_modality/methods/novel/train/script.py
+++ src/tasks/predict_modality/methods/novel/train/script.py
@@ -2,7 +2,7 @@ import sys
 
 import torch
 from torch.utils.data import DataLoader
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 
 import anndata as ad
 import pickle
@@ -19,9 +19,13 @@ else:
 ## VIASH START
 
 par = {
-  'input_train_mod1': 'resources_test/predict_modality/openproblems_neurips2021/bmmc_cite/train_mod1.h5ad',
-  'input_train_mod2': 'resources_test/predict_modality/openproblems_neurips2021/bmmc_cite/train_mod2.h5ad',
+  'input_train_mod1': 'resources_test/predict_modality/openproblems_neurips2021_normal/bmmc_cite/train_mod1.h5ad',
+  'input_train_mod2': 'resources_test/predict_modality/openproblems_neurips2021_normal/bmmc_cite/train_mod2.h5ad',
   'output': 'model.pt'
+}
+
+meta = {
+   'resources_dir': 'src/tasks/predict_modality/methods/novel',
 }
 ## VIASH END
 
@@ -30,7 +34,7 @@ sys.path.append(meta['resources_dir'])
 from helper_functions import train_and_valid, lsiTransformer, ModalityMatchingDataset
 from helper_functions import ModelRegressionAtac2Gex, ModelRegressionAdt2Gex, ModelRegressionGex2Adt, ModelRegressionGex2Atac
 
-print("Load data", flush=True)
+print('Load data', flush=True)
 
 # TODO: something went really wrong while refactoring this component,
 # because the train script is not supposed to get access to the test data.
@@ -40,32 +44,33 @@ print("Load data", flush=True)
 
 input_train_mod1 = ad.read_h5ad(par['input_train_mod1'])
 input_train_mod2 = ad.read_h5ad(par['input_train_mod2'])
-# input_test_mod1 = ad.read_h5ad(par['input_test_mod1'])
-# input_test_mod2 = ad.read_h5ad(par['input_test_mod2'])
 
 mod1 = input_train_mod1.uns['modality']
 mod2 = input_train_mod2.uns['modality']
 
 input_train_mod1.X = input_train_mod2.layers['counts']
 input_train_mod2.X = input_train_mod1.layers['counts']
-# input_test_mod1.X = input_test_mod1.layers['counts']
-# input_test_mod2.X = input_test_mod2.layers['counts']
 
-print("Start train", flush=True)
-if mod1 != "ADT":
-  input_train_mod2_df = input_train_mod2.to_df()
-  
+input_train_mod2_df = input_train_mod2.to_df()
+
+print('Start train', flush=True)
+if mod1 != 'ADT':  
   lsi_transformer_gex = lsiTransformer(n_components=256)
-  gex_train = lsi_transformer_gex.fit_transform(input_train_mod1)
-  
-  train_mod1, test_mod1, train_mod2, test_mod2 = train_test_split(gex_train, input_train_mod2_df, test_size=0.25, random_state=666)
-  input_train_mod2_df = input_train_mod2.to_df()
+  input_train_mod1_df = lsi_transformer_gex.fit_transform(input_train_mod1)
 else:
-  train_mod1 = input_train_mod1.to_df()
-  train_mod2 = input_train_mod2.to_df()
-  # test_mod1 = input_test_mod1.to_df()
-  # test_mod2 = input_test_mod2.to_df()
+  input_train_mod1_df = input_train_mod1.to_df()
 
+# reproduce train/test split from phase 1
+batch = input_train_mod1.obs["batch"]
+train_ix = [ k for k,v in enumerate(batch) if v not in {'s1d2', 's3d7'} ]
+test_ix = [ k for k,v in enumerate(batch) if v in {'s1d2', 's3d7'} ]
+
+train_mod1 = input_train_mod1_df.iloc[train_ix, :]
+train_mod2 = input_train_mod2_df.iloc[train_ix, :]
+test_mod1 = input_train_mod1_df.iloc[test_ix, :]
+test_mod2 = input_train_mod2_df.iloc[test_ix, :]
+  
+# train_mod1, test_mod1, train_mod2, test_mod2 = train_test_split(input_train_mod1, input_train_mod2, test_size=0.25, random_state=666)
 
 if mod1 == 'ATAC' and mod2 == 'GEX':
   dataset_train = ModalityMatchingDataset(train_mod1, train_mod2)
@@ -112,7 +117,7 @@ elif mod1 == 'GEX' and mod2 == 'ATAC':
 loss_fn = torch.nn.MSELoss()
 train_and_valid(model, optimizer, loss_fn, dataloader_train, dataloader_test, par['output'], device)
 
-if mod1 != "ADT":
+if mod1 != 'ADT':
     with open(par['output_pretrain'] + '/lsi_transformer.pickle', 'wb') as f:
         pickle.dump(lsi_transformer_gex, f)
 
