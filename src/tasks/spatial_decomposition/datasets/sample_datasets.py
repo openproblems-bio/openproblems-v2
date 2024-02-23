a9346b33c845428f3b630355d6d912d1c7275812
Nirmayi <ynirmayi@gmail.com>
2024-02-23 19:02:16 +0100

    generate synthetic spatial dataset in process_dataset when required

--- src/tasks/spatial_decomposition/datasets/sample_datasets.py
+++ src/tasks/spatial_decomposition/datasets/sample_datasets.py
@@ -8,7 +8,7 @@ from utils import filter_genes_cells
 def _pancreas_synthetic(
     adata: ad.AnnData,
     alpha: float,
-    n_obs: int = 100
+    n_obs: int = 100 
 ) -> ad.AnnData:
 
     adata.X = adata.layers["counts"]
@@ -17,18 +17,19 @@ def _pancreas_synthetic(
     adata.uns["spatial_data_summary"] = f"Dirichlet alpha={alpha}"
     filter_genes_cells(adata_merged)
     adata_merged.X = None
+    adata_merged.obs['is_primary_data'] = adata_merged.obs['is_primary_data'].fillna(False)
+
     return adata_merged
 
-def pancreas_alpha_1(adata, n_obs=100):
-    return _pancreas_synthetic(adata, n_obs=n_obs, alpha=1)
+# def pancreas_alpha_1(adata, n_obs=100):
+#     return _pancreas_synthetic(adata, n_obs=n_obs, alpha=1)
 
-def pancreas_alpha_5(adata, n_obs=100):
-    return _pancreas_synthetic(adata, n_obs=n_obs, alpha=5)
+# def pancreas_alpha_5(adata, n_obs=100):
+#     return _pancreas_synthetic(adata, n_obs=n_obs, alpha=5)
 
-def pancreas_alpha_0_5(adata, n_obs=100):
-    return _pancreas_synthetic(adata, n_obs=n_obs, alpha=0.5)
+# def pancreas_alpha_0_5(adata, n_obs=100):
+#     return _pancreas_synthetic(adata, n_obs=n_obs, alpha=0.5)
 
 if __name__ == "__main__":
-    adata = ad.read_h5ad(sys.argv[1])
-    pancreas = pancreas_alpha_1(adata, n_obs=100)
-    pancreas.write_h5ad(sys.argv[2])
+    adata = ad.read_h5ad("resources_test/common/cxg_mouse_pancreas_atlas/dataset.h5ad")
+    pancreas = _pancreas_synthetic(adata, n_obs=100, alpha=1)
