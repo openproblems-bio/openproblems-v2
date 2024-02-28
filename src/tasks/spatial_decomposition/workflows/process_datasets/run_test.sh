b4e1e6a9a042e597a1f330f71a19491f8a584c3e
Nirmayi <ynirmayi@gmail.com>
2024-02-28 15:24:27 +0100

    minor fixes

--- src/tasks/spatial_decomposition/workflows/process_datasets/run_test.sh
+++ src/tasks/spatial_decomposition/workflows/process_datasets/run_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run this prior to executing this script:
-# bin/viash_build -q 'batch_integration'
+# bin/viash_build -q 'spatial_decomposition'
 
 # get the root of the directory
 REPO_ROOT=$(git rev-parse --show-toplevel)
@@ -27,8 +27,8 @@ nextflow run . \
   -main-script target/nextflow/spatial_decomposition/workflows/process_datasets/main.nf \
   -profile docker \
   -entry auto \
-  --input_states "$RAW_DATA/**/state.yaml" \
+  -c src/wf_utils/labels_ci.config \
+  --input_states "resources_test/common/**/state.yaml" \
   --rename_keys 'input:output_dataset' \
-  --settings '{"output_spatial_masked": "$id/spatial_masked.h5ad", "output_single_cell": "$id/single_cell_ref.h5ad", "output_solution": "$id/solution.h5ad", "generate_dataset": true, "alpha": 1, "simulated_data": "$id/dataset_simulated.h5ad"}' \
-  --publish_dir "$DATASET_DIR" \
-  --output_state '$id/state.yaml'
+  --settings '{"output_spatial_masked": "$id/spatial_masked.h5ad", "output_single_cell": "$id/single_cell_ref.h5ad", "output_solution": "$id/solution.h5ad", "generate_dataset": true, "alpha": 1.0, "simulated_data": "$id/dataset_simulated.h5ad"}' \
+  --publish_dir "resources_test/spatial_decomposition"
