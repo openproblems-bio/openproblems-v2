4d208aa06ace0924eabd257a04faf6431a518099
Nirmayi <ynirmayi@gmail.com>
2024-02-28 14:31:48 +0100

    use default alpha 1.0

--- src/tasks/spatial_decomposition/resources_test_scripts/cxg_mouse_pancreas_atlas.sh
+++ src/tasks/spatial_decomposition/resources_test_scripts/cxg_mouse_pancreas_atlas.sh
@@ -32,9 +32,10 @@ nextflow run . \
   -main-script target/nextflow/spatial_decomposition/workflows/process_datasets/main.nf \
   -profile docker \
   -entry auto \
+  -c src/wf_utils/labels_ci.config \
   --input_states "$RAW_DATA/**/state.yaml" \
   --rename_keys 'input:output_dataset' \
-  --settings '{"output_spatial_masked": "$id/spatial_masked.h5ad", "output_single_cell": "$id/single_cell_ref.h5ad", "output_solution": "$id/solution.h5ad", "generate_dataset": true, "alpha": 1, "simulated_data": "$id/dataset_simulated.h5ad"}' \
+  --settings '{"output_spatial_masked": "$id/spatial_masked.h5ad", "output_single_cell": "$id/single_cell_ref.h5ad", "output_solution": "$id/solution.h5ad", "generate_dataset": true, "alpha": 1.0, "simulated_data": "$id/dataset_simulated.h5ad"}' \
   --publish_dir "$DATASET_DIR" \
   --output_state '$id/state.yaml'
 
