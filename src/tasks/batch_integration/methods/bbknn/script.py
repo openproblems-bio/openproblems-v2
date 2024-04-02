b47fc9deb452121fb5253346160dad89148dbfe9
Kai Waldrant <kai@data-intuitive.com>
2024-04-02 13:37:27 +0200

    refactor bbknn

--- src/tasks/batch_integration/methods/bbknn/script.py
+++ src/tasks/batch_integration/methods/bbknn/script.py
@@ -1,6 +1,5 @@
-import yaml
 import anndata as ad
-from scib.integration import bbknn
+import bbknn
 
 ## VIASH START
 par = {
@@ -15,13 +14,20 @@ meta = {
 ## VIASH END
 
 print('Read input', flush=True)
-input = ad.read_h5ad(par['input'])
+adata = ad.read_h5ad(par['input'])
 
 print('Run BBKNN', flush=True)
-input.X = input.layers['normalized']
-input = bbknn(input, batch='batch')
-del input.X
+kwargs = dict(batch_key='batch', copy=True)
+if adata.n_obs >= 1e5:
+    kwargs['neighbors_within_batch'] = 25
+
+ad_bbknn = bbknn.bbknn(adata, **kwargs)
+
+ad_out = adata.copy()
+ad_out.obsp['connectivities'] = ad_bbknn.obsp['connectivities']
+ad_out.obsp['distances'] = ad_bbknn.obsp['distances']
+ad_out.uns['neighbors'] = ad_bbknn.uns['neighbors']
+ad_out.uns['method_id'] = meta['functionality_name']
 
 print("Store outputs", flush=True)
-input.uns['method_id'] = meta['functionality_name']
-input.write_h5ad(par['output'], compression='gzip')
+ad_out.write_h5ad(par['output'], compression='gzip')
