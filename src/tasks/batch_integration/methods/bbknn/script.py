fb0e4d3be7cb78d292d54c373de43cdad9b4ee45
Kai Waldrant <kai@data-intuitive.com>
2024-04-03 11:03:57 +0200

    Update bbknn

--- src/tasks/batch_integration/methods/bbknn/script.py
+++ src/tasks/batch_integration/methods/bbknn/script.py
@@ -5,7 +5,9 @@ import bbknn
 par = {
     'input': 'resources_test/batch_integration/pancreas/unintegrated.h5ad',
     'output': 'output.h5ad',
-    'hvg': True,
+    'annoy_n_trees': 10,
+    'neighbors_within_batch': 3,
+    'n_hvg': 2000,
 }
 meta = {
     'functionality_name': 'foo',
@@ -16,18 +18,33 @@ meta = {
 print('Read input', flush=True)
 adata = ad.read_h5ad(par['input'])
 
+if par['n_hvg']:
+    print(f"Select top {par['n_hvg']} high variable genes", flush=True)
+    idx = adata.var['hvg_score'].to_numpy().argsort()[::-1][:par['n_hvg']]
+    adata = adata[:, idx].copy()
+
 print('Run BBKNN', flush=True)
 kwargs = dict(batch_key='batch', copy=True)
-if adata.n_obs >= 1e5:
-    kwargs['neighbors_within_batch'] = 25
+kwargs['annoy_n_trees'] = par['annoy_n_trees']
+kwargs['neighbors_within_batch'] = par['neighbors_within_batch']
 
 ad_bbknn = bbknn.bbknn(adata, **kwargs)
 
-ad_out = adata.copy()
-ad_out.obsp['connectivities'] = ad_bbknn.obsp['connectivities']
-ad_out.obsp['distances'] = ad_bbknn.obsp['distances']
-ad_out.uns['neighbors'] = ad_bbknn.uns['neighbors']
-ad_out.uns['method_id'] = meta['functionality_name']
+print("Store output", flush=True)
+output = ad.AnnData(
+    obs=adata.obs[[]],
+    var=adata.var[[]],
+    obsp={
+        'connectivities': ad_bbknn.obsp['connectivities'],
+        'distances': ad_bbknn.obsp['distances'],
+    },
+    uns={
+        'dataset_id': adata.uns['dataset_id'],
+        'normalization_id': adata.uns['normalization_id'],
+        'method_id': meta['functionality_name'],
+        'neighbors': ad_bbknn.uns['neighbors']
+    }
+)
 
 print("Store outputs", flush=True)
-ad_out.write_h5ad(par['output'], compression='gzip')
+output.write_h5ad(par['output'], compression='gzip')
