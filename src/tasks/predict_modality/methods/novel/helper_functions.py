88f081d171920722111e78ef03b287a74e621626
Kai Waldrant <kai@data-intuitive.com>
2024-04-19 13:02:04 +0200

    Remove unused code

--- src/tasks/predict_modality/methods/novel/helper_functions.py
+++ src/tasks/predict_modality/methods/novel/helper_functions.py
@@ -17,26 +17,6 @@ import sklearn.preprocessing
 import sklearn.neighbors
 import sklearn.utils.extmath
 
-def tfidf(X):
-    r"""
-    TF-IDF normalization (following the Seurat v3 approach)
-    Parameters
-    ----------
-    X
-        Input matrix
-    Returns
-    -------
-    X_tfidf
-        TF-IDF normalized matrix
-    """
-    idf = X.shape[0] / X.sum(axis=0)
-    if scipy.sparse.issparse(X):
-        tf = X.multiply(1 / X.sum(axis=1))
-        return tf.multiply(idf)
-    else:
-        tf = X / X.sum(axis=1, keepdims=True)
-        return tf * idf
-
 class tfidfTransformer():
     def __init__(self):
         self.idf = None
@@ -60,7 +40,6 @@ class tfidfTransformer():
         self.fit(X)
         return self.transform(X)
 
-
 class lsiTransformer():
     def __init__(self,
                  n_components: int = 20,
@@ -77,8 +56,8 @@ class lsiTransformer():
         
     def fit(self, adata: anndata.AnnData):
         if self.use_highly_variable is None:
-            self.use_highly_variable = "highly_variable" in adata.var
-        adata_use = adata[:, adata.var["highly_variable"]] if self.use_highly_variable else adata
+            self.use_highly_variable = "hvg" in adata.var
+        adata_use = adata[:, adata.var["hvg"]] if self.use_highly_variable else adata
         X = self.tfidfTransformer.fit_transform(adata_use.X)
         X_norm = self.normalizer.fit_transform(X)
         X_norm = np.log1p(X_norm * 1e4)
@@ -90,7 +69,7 @@ class lsiTransformer():
     def transform(self, adata):
         if not self.fitted:
             raise RuntimeError('Transformer was not fitted on any data')
-        adata_use = adata[:, adata.var["highly_variable"]] if self.use_highly_variable else adata
+        adata_use = adata[:, adata.var["hvg"]] if self.use_highly_variable else adata
         X = self.tfidfTransformer.transform(adata_use.X)
         X_norm = self.normalizer.transform(X)
         X_norm = np.log1p(X_norm * 1e4)
@@ -103,38 +82,6 @@ class lsiTransformer():
     def fit_transform(self, adata):
         self.fit(adata)
         return self.transform(adata)
-
-def lsi(
-        adata: anndata.AnnData, n_components: int = 20,
-        use_highly_variable: Optional[bool] = None, **kwargs
-) -> None:
-    r"""
-    LSI analysis (following the Seurat v3 approach)
-    Parameters
-    ----------
-    adata
-        Input dataset
-    n_components
-        Number of dimensions to use
-    use_highly_variable
-        Whether to use highly variable features only, stored in
-        ``adata.var['highly_variable']``. By default uses them if they
-        have been determined beforehand.
-    **kwargs
-        Additional keyword arguments are passed to
-        :func:`sklearn.utils.extmath.randomized_svd`
-    """
-    if use_highly_variable is None:
-        use_highly_variable = "highly_variable" in adata.var
-    adata_use = adata[:, adata.var["highly_variable"]] if use_highly_variable else adata
-    X = tfidf(adata_use.X)
-    X_norm = sklearn.preprocessing.Normalizer(norm="l1").fit_transform(X)
-    X_norm = np.log1p(X_norm * 1e4)
-    X_lsi = sklearn.utils.extmath.randomized_svd(X_norm, n_components, random_state=777, **kwargs)[0]
-    X_lsi -= X_lsi.mean(axis=1, keepdims=True)
-    X_lsi /= X_lsi.std(axis=1, ddof=1, keepdims=True)
-    adata.obsm["X_lsi"] = X_lsi
-
 
 class ModalityMatchingDataset(Dataset):
     def __init__(
@@ -155,7 +102,6 @@ class ModalityMatchingDataset(Dataset):
         else:
             x = self.df_modality1.iloc[index].values
             return x
-
 
 class Swish(torch.autograd.Function):
     @staticmethod
