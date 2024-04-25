3b382a0082584d04d1cb45ce85c7b743163d67f5
Robrecht Cannoodt <rcannood@gmail.com>
2024-04-25 20:53:21 +0200

    wip components

--- src/tasks/predict_modality/methods/simple_mlp/train/script.py
+++ src/tasks/predict_modality/methods/simple_mlp/train/script.py
@@ -6,6 +6,9 @@ logging.basicConfig(level=logging.INFO)
 
 ## VIASH START
 par = {
+    # 'input_train_mod1': 'resources/predict_modality/datasets/openproblems_neurips2021/bmmc_multiome/normal/log_cp10k/train_mod1.h5ad',
+    # 'input_train_mod2': 'resources/predict_modality/datasets/openproblems_neurips2021/bmmc_multiome/normal/log_cp10k/train_mod2.h5ad',
+    # 'input_test_mod1': 'resources/predict_modality/datasets/openproblems_neurips2021/bmmc_multiome/normal/log_cp10k/test_mod1.h5ad',
     'input_train_mod1': 'resources_test/predict_modality/openproblems_neurips2021/bmmc_multiome/swap/train_mod1.h5ad',
     'input_train_mod2': 'resources_test/predict_modality/openproblems_neurips2021/bmmc_multiome/swap/train_mod2.h5ad',
     'input_test_mod1': 'resources_test/predict_modality/openproblems_neurips2021/bmmc_multiome/swap/test_mod1.h5ad',
