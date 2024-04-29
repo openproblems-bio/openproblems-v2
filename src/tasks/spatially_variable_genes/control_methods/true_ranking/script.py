189c42e7f7ed6c5b7d2dfdc08e014d928ab6348b
lzj1769 <lzj1769@gmail.comz>
2024-04-28 21:13:13 -0400

    fix control method

--- src/tasks/spatially_variable_genes/control_methods/true_ranking/script.py
+++ src/tasks/spatially_variable_genes/control_methods/true_ranking/script.py
@@ -1,24 +1,29 @@
 import anndata as ad
-import pandas as pd
 
-## VIASH START
+# VIASH START
 par = {
-  'input_data': 'resources_test/spatially_variable_genes/10x_Visium_mouse_brain/input.h5ad',
-  'input_solution': 'resources_test/spatially_variable_genes/10x_Visium_mouse_brain/solution.csv',
-  'output': 'output.csv'
+    'input_data': 'resources_test/spatially_variable_genes/10x_Visium_mouse_brain/input.h5ad',
+    'input_solution': 'resources_test/spatially_variable_genes/10x_Visium_mouse_brain/solution.h5ad',
+    'output': 'output.h5ad'
 }
 meta = {
-  'functionality_name': 'true_ranking'
+    'functionality_name': 'true_ranking'
 }
-## VIASH END
+# VIASH END
 
 print('Reading input files', flush=True)
 input_data = ad.read_h5ad(par['input_data'])
 
 print('Generate predictions', flush=True)
-output = pd.read_csv(par['input_solution'])
+input_solution = ad.read_h5ad(par['input_solution'])
+
+df = input_solution.var
+df.columns = ['feature_name', 'gene_name', 'pred_spatial_var_score']
+
+output = ad.AnnData(var=df,
+                    uns={'dataset_id': '10x_Visium_mouse_brain',
+                         'method_id': 'true_ranking'})
+
 
 print("Write output to file", flush=True)
-output.columns = ['feature_name', 'gene_name', 'pred_spatial_var_score']
-
-output.to_csv(par['output'], index=False)
+output.write_h5ad(par['output'], index=False)
