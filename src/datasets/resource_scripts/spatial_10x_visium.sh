3c200c36995655ddaff77274aa4efa3cab9f2180
Nirmayi <ynirmayi@gmail.com>
2024-06-27 16:51:49 +0200

    update resource script

--- src/datasets/resource_scripts/spatial_10x_visium.sh
+++ src/datasets/resource_scripts/spatial_10x_visium.sh
@@ -11,10 +11,17 @@ param_list:
     dataset_description: "FFPE Mouse Brain tissue blocks sectioned as described in Visium CytAssist Spatial Gene Expression for FFPE - Tissue Preparation Guide Demonstrated Protocol. The H&E stained glass slide with tissue section was processed via Visium CytAssist instrument to transfer analytes to a Visium CytAssist Spatial Gene Expression slide. The probe extension and library construction steps follow the standard Visium for FFPE workflow outside of the instrument. The H&E image was acquired using Olympus VS200 Slide Scanning Microscope. Sequencing depth was 53,497 reads per spot. Sequencing configuration: 28bp read 1 (16bp Visium spatial barcode, 12bp UMI), 90bp read 2 (transcript), 10bp i7 sample barcode and 10bp i5 sample barcode. Key metrics include: 2,310 spots detected under tissue; 6,736 median genes per spot; 24,862 median UMI counts per spot."
     dataset_organism: Mus musculus
 
+normalization_methods: [log_cp10k]
+n_obs: 600
+n_vars: 500
 output_dataset: '$id/dataset.h5ad'
 output_meta: '$id/dataset_metadata.yaml'
 output_state: '$id/state.yaml'
+output_raw: force_null
+output_normalized: force_null
+output_hvg: force_null
 publish_dir: s3://openproblems-data/resources/datasets
+do_subsample: true
 HERE
 
 cat > /tmp/nextflow.config << HERE
