e8b73ba11c301d819300e0295eae12c1c300c637
Nirmayi <ynirmayi@gmail.com>
2024-07-24 22:26:33 +0200

    add dataset reference

--- src/datasets/resource_test_scripts/mouse_brain_coronal_section1.sh
+++ src/datasets/resource_test_scripts/mouse_brain_coronal_section1.sh
@@ -11,6 +11,7 @@ param_list:
     dataset_url: "https://www.10xgenomics.com/datasets/mouse-brain-coronal-section-1-ffpe-2-standard"
     dataset_summary: Gene expression library of Mouse Brain (CytAssist FFPE) using the Mouse Whole Transcriptome Probe Set
     dataset_description: "FFPE Mouse Brain tissue blocks sectioned as described in Visium CytAssist Spatial Gene Expression for FFPE - Tissue Preparation Guide Demonstrated Protocol. The H&E stained glass slide with tissue section was processed via Visium CytAssist instrument to transfer analytes to a Visium CytAssist Spatial Gene Expression slide. The probe extension and library construction steps follow the standard Visium for FFPE workflow outside of the instrument. The H&E image was acquired using Olympus VS200 Slide Scanning Microscope. Sequencing depth was 53,497 reads per spot. Sequencing configuration: 28bp read 1 (16bp Visium spatial barcode, 12bp UMI), 90bp read 2 (transcript), 10bp i7 sample barcode and 10bp i5 sample barcode. Key metrics include: 2,310 spots detected under tissue; 6,736 median genes per spot; 24,862 median UMI counts per spot."
+    dataset_reference: 10x2022brain
     dataset_organism: Mus musculus
 
 normalization_methods: [log_cp10k]
