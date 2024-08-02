3d8269f3f9939ef248b402e2d79833ffb58a6a57
Nirmayi <ynirmayi@gmail.com>
2024-08-02 16:47:15 +0200

    update input arguments

--- src/tasks/spatially_variable_genes/resources_scripts/process_datasets.sh
+++ src/tasks/spatially_variable_genes/resources_scripts/process_datasets.sh
@@ -30,7 +30,7 @@
 # cat > /tmp/params.yaml << 'HERE'
 # id: spatially_variable_genes_process_datasets
 # input_states: "s3://openproblems-data/resources/datasets/spatial_slideseq_v2/**/state.yaml"
-# settings: '{"output_dataset": "$id/dataset.h5ad", "output_solution": "$id/solution.h5ad", "dataset_simulated_normalized": "$id/simulated_dataset.h5ad", "gp_k_sim": 500, "select_top_variable_genes_sim": 50, "num_reference_genes": 200}'
+# settings: '{"output_dataset": "$id/dataset.h5ad", "output_solution": "$id/solution.h5ad", "dataset_simulated_normalized": "$id/simulated_dataset.h5ad", "gp_k_sim": 500, "select_top_variable_genes_sim": 20, "num_reference_genes": 25}'
 # rename_keys: 'input:output_dataset'
 # output_state: "$id/state.yaml"
 # publish_dir: s3://openproblems-data/resources/spatially_variable_genes/datasets
@@ -45,10 +45,19 @@
 # publish_dir: s3://openproblems-data/resources/spatially_variable_genes/datasets
 # HERE
 
+# cat > /tmp/params.yaml << 'HERE'
+# id: spatially_variable_genes_process_datasets
+# input_states: "s3://openproblems-data/resources/datasets/spatial_merfish/**/state.yaml"
+# settings: '{"output_dataset": "$id/dataset.h5ad", "output_solution": "$id/solution.h5ad", "dataset_simulated_normalized": "$id/simulated_dataset.h5ad", "gp_k_sim": 500, "select_top_variable_genes_sim": 50, "num_reference_genes": 200}'
+# rename_keys: 'input:output_dataset'
+# output_state: "$id/state.yaml"
+# publish_dir: s3://openproblems-data/resources/spatially_variable_genes/datasets
+# HERE
+
 cat > /tmp/params.yaml << 'HERE'
 id: spatially_variable_genes_process_datasets
-input_states: "s3://openproblems-data/resources/datasets/spatial_merfish/**/state.yaml"
-settings: '{"output_dataset": "$id/dataset.h5ad", "output_solution": "$id/solution.h5ad", "dataset_simulated_normalized": "$id/simulated_dataset.h5ad", "gp_k_sim": 500, "select_top_variable_genes_sim": 50, "num_reference_genes": 200}'
+input_states: "s3://openproblems-data/resources/datasets/spatial_seqfish/**/state.yaml"
+settings: '{"output_dataset": "$id/dataset.h5ad", "output_solution": "$id/solution.h5ad", "dataset_simulated_normalized": "$id/simulated_dataset.h5ad", "gp_k_sim": 500, "select_top_variable_genes_sim": 25, "num_reference_genes": 50}'
 rename_keys: 'input:output_dataset'
 output_state: "$id/state.yaml"
 publish_dir: s3://openproblems-data/resources/spatially_variable_genes/datasets
@@ -56,17 +65,8 @@ HERE
 
 # cat > /tmp/params.yaml << 'HERE'
 # id: spatially_variable_genes_process_datasets
-# input_states: "s3://openproblems-data/resources/datasets/spatial_seqfish/**/state.yaml"
-# settings: '{"output_dataset": "$id/dataset.h5ad", "output_solution": "$id/solution.h5ad", "dataset_simulated_normalized": "$id/simulated_dataset.h5ad", "gp_k_sim": 500, "select_top_variable_genes_sim": 50, "num_reference_genes": 200}'
-# rename_keys: 'input:output_dataset'
-# output_state: "$id/state.yaml"
-# publish_dir: s3://openproblems-data/resources/spatially_variable_genes/datasets
-# HERE
-
-# cat > /tmp/params.yaml << 'HERE'
-# id: spatially_variable_genes_process_datasets
 # input_states: "s3://openproblems-data/resources/datasets/spatial_star_map/**/state.yaml"
-# settings: '{"output_dataset": "$id/dataset.h5ad", "output_solution": "$id/solution.h5ad", "dataset_simulated_normalized": "$id/simulated_dataset.h5ad", "gp_k_sim": 500, "select_top_variable_genes_sim": 50, "num_reference_genes": 25}'
+# settings: '{"output_dataset": "$id/dataset.h5ad", "output_solution": "$id/solution.h5ad", "dataset_simulated_normalized": "$id/simulated_dataset.h5ad", "gp_k_sim": 500, "select_top_variable_genes_sim": 25, "num_reference_genes": 25}'
 # rename_keys: 'input:output_dataset'
 # output_state: "$id/state.yaml"
 # publish_dir: s3://openproblems-data/resources/spatially_variable_genes/datasets
